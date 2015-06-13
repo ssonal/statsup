9ec667ee3ee7a9e285ac78037399b7bd28665100
Sonal <sonal.saldanha@gmail.com>
2015-06-13 11:11:00 +0530

    Histograms, colors, cumulative graphs

--- statsup.py
+++ statsup.py
@@ -9,17 +9,12 @@ import math
 from datetime import date
 
 
-colors=['#D3132D', '#1527F5']
+colors=['#D3132D', '#1527F5', '#1F76B4','#903864','#D61E28', '#C8C8C8', '#8C582D']
 
-
-colors = ['#1F76B4','#903864','#D61E28', '#C8C8C8', '#8C582D']
 month = {'Jan':1,'Feb':2,'Mar':3,'Apr':4,'May':5,'Jun':6,'Jul':7,'Aug':8,'Sep':9,'Oct':10,'Nov':11,'Dec':12}
 reMonth = {v:k for k,v in month.items()}
-name = {} # {"Name1":0,"Name2":1,...} Integers are assigned in chronological order of first message sent
-nmonths = 0 # Number of months for which the conversation has been recorded
-sdate,edate = [], [] # Stores the start and end date respectively
-msgs = [] # [[date, Time], person, message]
-pl = [] # Person List
+name = {}
+
 
 def monthsBetween(sdate, edate):
 	if not (edate[2] == sdate[2]):
@@ -35,19 +30,14 @@ def hourlyG(hourlyC,names):
 	hourlyC = hourlyC/row_sums[:, np.newaxis]
 	hour = np.arange(0,24)
 	width = 0.35
-<<<<<<< HEAD
-	fig,ax = plt.subplots()
-=======
->>>>>>> cf6bd0fc814ccdc9f3a2a2cd3030c43f7756727f
 	bars = []
 
 	fig,ax = plt.subplots()
 
-
 	for i in range(len(names)):
-		b = ax.bar(hour+(i*width), hourlyC[i,:], width/(len(names)-1), color=colors[i],alpha=0.5)
+		b = ax.bar(hour+(i*width), hourlyC[i,:], width/(len(names)-1), color=colors[i], alpha=0.75)
 		bars.append(b[0])
-
+	
 	ax.spines["top"].set_visible(False)  
 	ax.spines["right"].set_visible(False)  
 	ax.get_xaxis().tick_bottom()
@@ -55,7 +45,7 @@ def hourlyG(hourlyC,names):
 
 	ax.set_ylabel('Percentage of Messages sent')
 	ax.set_xlabel('Hour o\' Day')
-
+	
 	ax.set_xticks(hour+width)
 	h = np.array(hour, dtype=str)
 	ax.set_xticklabels(h)
@@ -69,29 +59,22 @@ def hourlyG(hourlyC,names):
 def monthlyG(monthlyC, names):
 	row_sums = monthlyC.sum(axis=1)
 	monthlyC = monthlyC/row_sums[:, np.newaxis]
-<<<<<<< HEAD
-
-=======
->>>>>>> cf6bd0fc814ccdc9f3a2a2cd3030c43f7756727f
 	mrange = np.arange(0,nmonths)
-	print mrange
 	width = 0.35
+	
 
 	fig,ax = plt.subplots()
-<<<<<<< HEAD
-=======
-	
->>>>>>> cf6bd0fc814ccdc9f3a2a2cd3030c43f7756727f
 	bars = []
+	
 	for i in range(len(names)):
-		b = ax.bar(mrange+(i*width), monthlyC[i,:], width/(len(names)-1), color=colors[i],alpha=0.75)
+		b = ax.bar(mrange+(i*width), monthlyC[i,:], width/(len(names)-1), color=colors[i], alpha=0.75)
 		bars.append(b[0])
 	
 	ax.spines["top"].set_visible(False)  
 	ax.spines["right"].set_visible(False)  
 	ax.get_xaxis().tick_bottom()
-	ax.get_yaxis().tick_left()	
-	
+	ax.get_yaxis().tick_left()
+
 	ax.set_ylabel('Percentage of Messages sent')
 	ax.set_xlabel('Month o\' Year')
 	ax.set_xticks(mrange+width)
@@ -100,7 +83,7 @@ def monthlyG(monthlyC, names):
 	i = 1
 	mlabels = ['' for x in range(nmonths)]
 	mlabels[0] = sm+' \''+sy[-2:]
-
+	
 	if sm == 'Dec':
 		sm = 'Jan'
 		sy = str(int(sy)+1)
@@ -112,6 +95,7 @@ def monthlyG(monthlyC, names):
 			mlabels[i] = sm+' \''+sy[-2:]
 		else:
 			mlabels[i] = sm
+		
 		if sm == 'Dec':
 			sm = 'Jan'
 			sy = str(int(sy)+1)
@@ -131,34 +115,20 @@ def monthlyG(monthlyC, names):
 	plt.show()
 
 
-<<<<<<< HEAD
-
-
-term = sys.stdin
-sys.stdin = open('./test files/mojo.txt')
-sys.stdout = open('b.out','w')
-i = 0
-msgs = []
-=======
 ##########################
 ### Message Extraction ###
 ##########################
-term = sys.stdin
+# term = sys.stdin
 sys.stdin = open('./test files/mojo.txt')
+# sys.stdout = open('b.out','w')
+i = 0
+msgs = []
 
-
->>>>>>> cf6bd0fc814ccdc9f3a2a2cd3030c43f7756727f
-inp = sys.stdin.read()
-
-inputs = re.split(r'\n(?=[\w]{3} [\d]+, [\d, ]*?[\d]+:[\d]+ [A|P]M)', inp)
+inputs = re.split(r'\n(?=[\w]{3} [\d]+, [\d, ]*?[\d]+:[\d]+ [A|P]M)', sys.stdin.read())
 sys.stdin.close()
-sys.stdin = term
+# sys.stdin = term
 
-<<<<<<< HEAD
-# format - [[date Time], person, message]
-=======
-# format - [[date, Time], person, message]
->>>>>>> cf6bd0fc814ccdc9f3a2a2cd3030c43f7756727f
+# format - [[date, time], person, message]
 for line in inputs:
 	dt = re.match(r'^.*M (?=-)', line).group()
 	dt = ''.join(dt.split(',')).strip()
@@ -175,7 +145,7 @@ for line in inputs:
 
 	msgs +=[(dt, pe.strip(), me.strip())]
 
-# Populate Person List
+# Populate person list
 pl = [msgs[0][1]]
 for msg in msgs[1:]:
 	if not msg[1] in pl:
@@ -192,16 +162,13 @@ for n in pl:
 sdate,edate = msgs[0][0][0].split(' '),msgs[-1][0][0].split(' ')
 nmonths = monthsBetween(sdate,edate)
 
-<<<<<<< HEAD
-hourlyC = np.zeros([len(ll),24], dtype=float)
-monthlyC = np.zeros([len(ll),nmonths], dtype=float)
-dailyC = [[] for _ in range(len(ll))]
-currentDate = msgs[0][0][0]
-i = 0
-=======
 hourlyC = np.zeros([len(pl),24], dtype=float)
 monthlyC = np.zeros([len(pl),nmonths], dtype=float)
->>>>>>> cf6bd0fc814ccdc9f3a2a2cd3030c43f7756727f
+dailyC = [[] for _ in range(len(pl))]
+
+
+currentDate = msgs[0][0][0]
+i = 0
 
 for msg in msgs:
 	d = msg[0][0]
@@ -234,14 +201,9 @@ for msg in msgs:
 	dailyC[name[sender]] += [i]
 	monthlyC[name[sender]][off] += 1
 
-<<<<<<< HEAD
-hourlyG(hourlyC, ll)
-monthlyG(monthlyC, ll)
-
-plt.figure()
-plt.hist([dailyC[0], dailyC[1]], bins=207,normed=1, histtype='step',color=colors, cumulative=0, alpha=1)
-plt.show()
-=======
 hourlyG(hourlyC, pl)
 monthlyG(monthlyC, pl)
->>>>>>> cf6bd0fc814ccdc9f3a2a2cd3030c43f7756727f
+
+plt.figure()
+plt.hist([dailyC[0], dailyC[1]], bins=207,normed=1, histtype='step',color=colors[:len(pl)], cumulative=0, alpha=1)
+plt.show()
