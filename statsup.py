440202f9ddd89c5aa50c5054b2403853f7764a91
Sonal <sonal.saldanha@gmail.com>
2015-06-13 10:37:57 +0530

    Histogram addition. Daily messaging trends

--- statsup.py
+++ statsup.py
@@ -1,17 +1,22 @@
-
-
 import sys
 import numpy as np
 import re
 import time
 import json
 import matplotlib.pyplot as plt
+import math
+
+from datetime import date
+
+
+colors=['#D3132D', '#1527F5']
 
 def monthsBetween(sdate, edate):
 	if not (edate[2] == sdate[2]):
 		return (12 - month[sdate[0]]+1) + month[edate[0]] + 12*(int(edate[2])-int(sdate[2])-1)
 	else:
-		return month[edate[0]] - month[sdate[0]] + 1
+		return int(math.fabs(month[edate[0]] - month[sdate[0]]) + 1)
+
 ############
 ## Hourly ##
 ############
@@ -19,11 +24,8 @@ def hourlyG(hourlyC,names):
 	row_sums = hourlyC.sum(axis=1)
 	hourlyC = hourlyC/row_sums[:, np.newaxis]
 	hour = np.arange(0,24)
-	# print hourlyC
-	# print hourlyC[:,0]
 	width = 0.35
 	fig,ax = plt.subplots()
-	colors = ['b','y']
 	bars = []
 	for i in range(len(names)):
 		b = ax.bar(hour+(i*width), hourlyC[i,:], width/(len(names)-1), color=colors[i])
@@ -45,13 +47,11 @@ def hourlyG(hourlyC,names):
 def monthlyG(monthlyC, names):
 	row_sums = monthlyC.sum(axis=1)
 	monthlyC = monthlyC/row_sums[:, np.newaxis]
-	print nmonths
-	# print hourlyC
-	# print hourlyC[:,0]
+
 	mrange = np.arange(0,nmonths)
+	print mrange
 	width = 0.35
 	fig,ax = plt.subplots()
-	colors = ['b','y']
 	bars = []
 	for i in range(len(names)):
 		b = ax.bar(mrange+(i*width), monthlyC[i,:], width/(len(names)-1), color=colors[i])
@@ -84,9 +84,12 @@ def monthlyG(monthlyC, names):
 	# plt.title('Title')
 	plt.show()
 
+
+
+
 term = sys.stdin
 sys.stdin = open('./test files/mojo.txt')
-# sys.stdout = open('b.out','w')
+sys.stdout = open('b.out','w')
 i = 0
 msgs = []
 inp = sys.stdin.read()
@@ -94,10 +97,8 @@ inp = sys.stdin.read()
 inputs = re.split(r'\n(?=[\w]{3} [\d]+, [\d, ]*?[\d]+:[\d]+ [A|P]M)', inp)
 sys.stdin.close()
 sys.stdin = term
-# print inputs
-# print len(inputs)
-# format - [[date Time], person, message]
 
+# format - [[date Time], person, message]
 for line in inputs:
 	dt = re.match(r'^.*M (?=-)', line).group()
 	dt = ''.join(dt.split(',')).strip()
@@ -134,6 +135,9 @@ nmonths = monthsBetween(sdate,edate)
 
 hourlyC = np.zeros([len(ll),24], dtype=float)
 monthlyC = np.zeros([len(ll),nmonths], dtype=float)
+dailyC = [[] for _ in range(len(ll))]
+currentDate = msgs[0][0][0]
+i = 0
 
 for msg in msgs:
 	d = msg[0][0]
@@ -144,18 +148,34 @@ for msg in msgs:
 	# mon = abs(month[d.split(' ')[0]] - month[sdate[0]]) + 12*(int(d.split(' ')[2])-int(sdate[2]))
 	# day = int(d.split(' ')[1])
 	off = monthsBetween(sdate,d.split(' ')) - 1
-	# print off
-	# raw_input()
+
 	if ap == 'AM':
 		if h == 12: h = 0	
 	else:
 		if not h == 12: h += 12
 
-	hourlyC[name[sender]][h%24] += 1
-	monthlyC[name[sender]][off] += 1
-	# dailyC[msg[1]][day] += 1
+	if not d == currentDate:
+		m0, d0, y0 = currentDate.split(' ')
+		m0 = month[m0]
+		d0 = int(d0)
+		y0 = int(y0)
 
-# print name
-# print monthlyC
+		m1, d1, y1 = d.split(' ')
+		m1 = month[m1]
+		d1 = int(d1)
+		y1 = int(y1)
+
+		diff = date(y1,m1,d1)-date(y0,m0,d0)
+		i += diff.days
+		currentDate = d
+	
+	hourlyC[name[sender]][h%24] += 1
+	dailyC[name[sender]] += [i]
+	monthlyC[name[sender]][off] += 1
+
 hourlyG(hourlyC, ll)
 monthlyG(monthlyC, ll)
+
+plt.figure()
+plt.hist([dailyC[0], dailyC[1]], bins=207,normed=1, histtype='step',color=colors, cumulative=0, alpha=1)
+plt.show()
