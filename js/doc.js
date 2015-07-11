1dd23c33fdaa215c102e6a54fce6852571d3624a
Sonal <sonal.saldanha@gmail.com>
2015-07-11 16:02:32 +0530

    changes to handle new whatsapp export format

--- js/doc.js
+++ js/doc.js
@@ -1,24 +1,26 @@
 
 var preProcess = function(data) {
 	data = data.split(/\n(?=[\w]{3} [\d]+, [\d, ]*?[\d]+:[\d]+ [A|P]M)/);
-	var oneDay = 24*60*60*1000;
 
-	var months = {'Jan':1, 'Feb':2,'Mar':3,'Apr':4,'May':5,'Jun':6,'Jul':7,'Aug':8,'Sep':9,'Oct':10, 'Nov':11, 'Dec':12}
+	if(data.length == 1) 
+		data = data[0].split(/\n(?=[\d]+\/[\d]+\/[\d]{4}, [\d]+:[\d]+ [A|P]M)/)
+	// var oneDay = 24*60*60*1000;
+	// var months = {'Jan':1, 'Feb':2,'Mar':3,'Apr':4,'May':5,'Jun':6,'Jul':7,'Aug':8,'Sep':9,'Oct':10, 'Nov':11, 'Dec':12}
 
 	var msgs = _.map(data, function(line) {
 		var dateTime = line.match(/^.*M (?=-)/)[0].split(',').join('').trim().split(' ');
-		
-		if(dateTime.length < 5)
+
+		if(dateTime.length == 4)
 			dateTime.splice(2,0, new Date().getFullYear());
+		
+		dateTime = moment(dateTime, ["MMM DD YYYY hh:mm A", "MM/DD/YYYY hh:mm A"])
 
-		var date = dateTime.slice(0,3);
-		date[0] = months[date[0]].toString();
-
-		dateTime = [date.join(' ').trim(), dateTime.slice(3,dateTime.length).join(' ').trim()]
+		// var date = dateTime.slice(0,3);
+		// date[0] = months[date[0]].toString();
+		// dateTime = [date.join(' ').trim(), dateTime.slice(3,dateTime.length).join(' ').trim()]
 
 		var person = line.match(/- ([\w ]+(?=:))/)[1].trim();
 		var message = line.match( /: (.*)/)[1].trim();
-
 		return [dateTime, person, message];
 	});
 
@@ -27,46 +29,54 @@ var preProcess = function(data) {
 				  .uniq()
 				  .value();
 
+  	// function dateDiff(d1, d2) {
+	// 	d1 = _.map(d1.split(' '), function(item) { return parseInt(item); })
+	// 	d2 = _.map(d2.split(' '), function(item) { return parseInt(item); })
 
-  	function dateDiff(d1, d2) {
-		d1 = _.map(d1.split(' '), function(item) { return parseInt(item); })
-		d2 = _.map(d2.split(' '), function(item) { return parseInt(item); })
+	// 	d1 = new Date(d1[2],d1[0],d1[1]);
+	// 	d2 = new Date(d2[2],d2[0],d2[1]);
 
-		d1 = new Date(d1[2],d1[0],d1[1]);
-		d2 = new Date(d2[2],d2[0],d2[1]);
-
-		return Math.round(Math.abs((d1.getTime() - d2.getTime())/(oneDay)));
-	}
+	// 	return Math.round(Math.abs((d1.getTime() - d2.getTime())/(oneDay)));
+	// }
   	var jsonData = {}; 
 
-  	_.each(people, function(person) { jsonData[person] = {'hourly': math.zeros(24).toArray(), 'daily': math.zeros(dateDiff(msgs[0][0][0], _.last(msgs)[0][0])+1).toArray()} });
+  	_.each(people, function(person) { jsonData[person] = {'hourly': math.zeros(24).toArray(), 'daily': math.zeros(Math.floor(moment.duration(_.last(msgs)[0].diff(msgs[0][0])).asDays())+1).toArray()} });
+  	// _.each(people, function(person) { jsonData[person] = {'hourly': math.zeros(24).toArray(), 'daily': math.zeros(dateDiff(msgs[0][0][0], _.last(msgs)[0][0])+1).toArray()} });
 
 	var messageCorpus = _.map(people, function(p) { return [] }),
 		i = 0,
-		currentDate = msgs[0][0][0], 
-		currentTime = msgs[0][0][1]
+		currentDate = msgs[0][0] 
+		// currentDate = msgs[0][0][0], 
+		// currentTime = msgs[0][0][1]
 
 	
 	_.each(msgs, function(msg) {
-		var date = msg[0][0]
-		var ap = _.last(msg[0][1], 2).join('')
-		var hour = parseInt(msg[0][1].match(/\d+(?=\:)/))		
+		var dateTime = msg[0]
+		// var date = msg[0][0]
+		// var ap = _.last(msg[0][1], 2).join('')
+		// var hour = parseInt(msg[0][1].match(/\d+(?=\:)/))		
 		var sender = msg[1]		
 
-		if (ap === "AM"){
-			if (hour == 12)
-				hour = 0
-		}else {
-			if (hour != 12)
-				hour += 12
+		// if (ap === "AM"){
+		// 	if (hour == 12)
+		// 		hour = 0
+		// }else {
+		// 	if (hour != 12)
+		// 		hour += 12
+		// }
+
+		// if(JSON.stringify(currentDate) != JSON.stringify(date)) {
+		// 	i += dateDiff(date, currentDate);
+		// 	currentDate = date.slice();
+		// }
+
+		if(currentDate.dayOfYear() != dateTime.dayOfYear()) {
+			i += Math.floor(moment.duration(dateTime.diff(currentDate)).asDays());
+			currentDate = dateTime.clone();
 		}
 
-		if(JSON.stringify(currentDate) != JSON.stringify(date)) {
-			i += dateDiff(date, currentDate);
-			currentDate = date.slice();
-		}
-
-		jsonData[sender]['hourly'][hour%24] += 1
+		jsonData[sender]['hourly'][dateTime.hour()] += 1
+		// jsonData[sender]['hourly'][hour%24] += 1
 		jsonData[sender]['daily'][i] += 1
 	});
 
