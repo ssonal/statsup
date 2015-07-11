ebd7273c5d7b907042ed4bceea6315dc5a154b40
Nikhil Saldanha <nikhil.saldanha691@gmail.com>
2015-07-12 00:22:13 +0530

    Hide graph layout prior to upload and group message support

--- js/doc.js
+++ js/doc.js
@@ -2,7 +2,7 @@
 var preProcess = function(data) {
 	data = data.split(/\n(?=[\w]{3} [\d]+, [\d, ]*?[\d]+:[\d]+ [A|P]M)/);
 
-	if(data.length == 1) 
+	if(data.length == 1)
 		data = data[0].split(/\n(?=[\d]+\/[\d]+\/[\d]{4}, [\d]+:[\d]+ [A|P]M)/)
 	// var oneDay = 24*60*60*1000;
 	// var months = {'Jan':1, 'Feb':2,'Mar':3,'Apr':4,'May':5,'Jun':6,'Jul':7,'Aug':8,'Sep':9,'Oct':10, 'Nov':11, 'Dec':12}
@@ -12,18 +12,22 @@ var preProcess = function(data) {
 
 		if(dateTime.length == 4)
 			dateTime.splice(2,0, new Date().getFullYear());
-		
+
 		dateTime = moment(dateTime, ["MMM DD YYYY hh:mm A", "MM/DD/YYYY hh:mm A"])
 
 		// var date = dateTime.slice(0,3);
 		// date[0] = months[date[0]].toString();
 		// dateTime = [date.join(' ').trim(), dateTime.slice(3,dateTime.length).join(' ').trim()]
-
-		var person = line.match(/- ([\w ]+(?=:))/)[1].trim();
+		try {
+			var person = line.match(/- ([\w ]+(?=:))/)[1].trim();
+		}
+		catch(e) {
+			return null;
+		}
 		var message = line.match( /: (.*)/)[1].trim();
 		return [dateTime, person, message];
 	});
-
+	msgs = _.filter(msgs, function(msg){return msg != null})
 	var people = _.chain(msgs)
 				  .map(function(msg) { return msg[1]; })
 				  .uniq()
@@ -38,24 +42,24 @@ var preProcess = function(data) {
 
 	// 	return Math.round(Math.abs((d1.getTime() - d2.getTime())/(oneDay)));
 	// }
-  	var jsonData = {}; 
+  	var jsonData = {};
 
   	_.each(people, function(person) { jsonData[person] = {'hourly': math.zeros(24).toArray(), 'daily': math.zeros(Math.floor(moment.duration(_.last(msgs)[0].diff(msgs[0][0])).asDays())+1).toArray()} });
   	// _.each(people, function(person) { jsonData[person] = {'hourly': math.zeros(24).toArray(), 'daily': math.zeros(dateDiff(msgs[0][0][0], _.last(msgs)[0][0])+1).toArray()} });
 
 	var messageCorpus = _.map(people, function(p) { return [] }),
 		i = 0,
-		currentDate = msgs[0][0] 
-		// currentDate = msgs[0][0][0], 
+		currentDate = msgs[0][0]
+		// currentDate = msgs[0][0][0],
 		// currentTime = msgs[0][0][1]
 
-	
+
 	_.each(msgs, function(msg) {
 		var dateTime = msg[0]
 		// var date = msg[0][0]
 		// var ap = _.last(msg[0][1], 2).join('')
-		// var hour = parseInt(msg[0][1].match(/\d+(?=\:)/))		
-		var sender = msg[1]		
+		// var hour = parseInt(msg[0][1].match(/\d+(?=\:)/))
+		var sender = msg[1]
 
 		// if (ap === "AM"){
 		// 	if (hour == 12)
@@ -92,13 +96,16 @@ function doOpen(evt) {
 		var data = this.result;
 
     	// showout.value = data;
+			d3.selectAll("svg").remove();
+			document.getElementById("lytics").style.display = "block";
+			
     	data = preProcess(data);
     	plot(data)
     };
-    
+
     reader.readAsBinaryString(files[0]);
 }
-    	
+
 var openbtn = document.getElementById("openselect")
     // showout = document.getElementById("showresult");
 openselect.addEventListener("change", doOpen, false);
