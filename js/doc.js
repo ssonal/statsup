4dabc4885191f51cc00f6a391397739ac9147f56
Sonal <sonal.saldanha@gmail.com>
2015-07-12 11:37:00 +0530

    Merge additions

--- js/doc.js
+++ js/doc.js
@@ -1,19 +1,24 @@
 
 var preProcess = function(data) {
-	data = data.split(/\n(?=[\w]{3} [\d]+, [\d, ]*?[\d]+:[\d]+ [A|P]M)/);
-
-	if(data.length == 1)
-		data = data[0].split(/\n(?=[\d]+\/[\d]+\/[\d]{4}, [\d]+:[\d]+ [A|P]M)/)
+	var dateFormat = "MMM DD YYYY hh:mm A"
+	
 	// var oneDay = 24*60*60*1000;
 	// var months = {'Jan':1, 'Feb':2,'Mar':3,'Apr':4,'May':5,'Jun':6,'Jul':7,'Aug':8,'Sep':9,'Oct':10, 'Nov':11, 'Dec':12}
+	data = data.split(/\n(?=[\w]{3} [\d]+, [\d, ]*?[\d]+:[\d]+ [A|P]M)/);
+	
+	if(data.length == 1)
+		data = data[0].split(/\n(?=[\d]+\/[\d]+\/[\d]{4}, [\d]+:[\d]+ [A|P]M)/)
 
+	if (!(moment(data[0].match(/^.*M (?=-)/)[0].split(',').join('').trim().split(' '), dateFormat).isValid()))
+		dateFormat = "MM/DD/YYYY hh:mm A"
+	
 	var msgs = _.map(data, function(line) {
 		var dateTime = line.match(/^.*M (?=-)/)[0].split(',').join('').trim().split(' ');
 
 		if(dateTime.length == 4)
 			dateTime.splice(2,0, new Date().getFullYear());
 
-		dateTime = moment(dateTime, ["MMM DD YYYY hh:mm A", "MM/DD/YYYY hh:mm A"])
+		dateTime = moment(dateTime, dateFormat)
 
 		// var date = dateTime.slice(0,3);
 		// date[0] = months[date[0]].toString();
@@ -25,13 +30,22 @@ var preProcess = function(data) {
 			return null;
 		}
 		var message = line.match( /: (.*)/)[1].trim();
+		
 		return [dateTime, person, message];
 	});
+	
 	msgs = _.filter(msgs, function(msg){return msg != null})
 	var people = _.chain(msgs)
 				  .map(function(msg) { return msg[1]; })
 				  .uniq()
 				  .value();
+
+  	function dateDiff(d1, d2) {
+		var m = d1.dayOfYear() - d2.dayOfYear()
+		if (m < 0)
+			return Math.ceil(moment.duration(d1.diff(d2)).asDays())
+		return m
+	}
 
   	// function dateDiff(d1, d2) {
 	// 	d1 = _.map(d1.split(' '), function(item) { return parseInt(item); })
@@ -44,8 +58,7 @@ var preProcess = function(data) {
 	// }
   	var jsonData = {};
 
-  	_.each(people, function(person) { jsonData[person] = {'hourly': math.zeros(24).toArray(), 'daily': math.zeros(Math.floor(moment.duration(_.last(msgs)[0].diff(msgs[0][0])).asDays())+1).toArray()} });
-  	// _.each(people, function(person) { jsonData[person] = {'hourly': math.zeros(24).toArray(), 'daily': math.zeros(dateDiff(msgs[0][0][0], _.last(msgs)[0][0])+1).toArray()} });
+  	_.each(people, function(person) { jsonData[person] = {'hourly': math.zeros(24).toArray(), 'daily': math.zeros(dateDiff(_.last(msgs)[0], msgs[0][0])+1).toArray()} });
 
 	var messageCorpus = _.map(people, function(p) { return [] }),
 		i = 0,
@@ -75,7 +88,7 @@ var preProcess = function(data) {
 		// }
 
 		if(currentDate.dayOfYear() != dateTime.dayOfYear()) {
-			i += Math.floor(moment.duration(dateTime.diff(currentDate)).asDays());
+			i += dateDiff(dateTime, currentDate);
 			currentDate = dateTime.clone();
 		}
 
